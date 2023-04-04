72111223511d30c92dec924ce39a58d9dabc83a8
KGRANGER <kevin.granger@ynov.com>
2023-04-04 14:35:18 +0200

    Event Manager / StateManager WIP

--- MarioLike/src/engine/GameEngine.cpp
+++ MarioLike/src/engine/GameEngine.cpp
@@ -17,6 +17,7 @@ GameEngine* GameEngine::GetInstance()
 GameEngine::~GameEngine()
 {
 	delete m_engine;
+	delete m_window;
 }
 void GameEngine::Start()
 {
---
+++ MarioLike/src/manager/StateManager.cpp
@@ -0,0 +1,2 @@
+#include "manager/StateManager.h"
+
